3e1b8081333fa9da99125ce855def943727653ca
hansen1416 <badapplesweetie@gmail.com>
2023-07-27 14:24:14 +0800

    1

--- rl/run.py
+++ rl/run.py
@@ -1,21 +1,14 @@
+import os
+
 import gymnasium as gym
 import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.ppo.policies import MlpPolicy
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
-# Set up fake display; otherwise rendering will fail
-import os
-import base64
-from pathlib import Path
 
-os.system("Xvfb :1 -screen 0 1024x768x24 &")
-os.environ['DISPLAY'] = ':1'
+from utils import record_video
 
-env = gym.make("CartPole-v1")
-
-model = PPO(MlpPolicy, env, verbose=0)
 
 def evaluate(
     model: BaseAlgorithm,
@@ -53,14 +46,22 @@ def evaluate(
         all_episode_rewards.append(sum(episode_rewards))
 
     mean_episode_reward = np.mean(all_episode_rewards)
-    print(f"Mean reward: {mean_episode_reward:.2f} - Num episodes: {num_episodes}")
+    print(
+        f"Mean reward: {mean_episode_reward:.2f} - Num episodes: {num_episodes}")
 
     return mean_episode_reward
 
-# Random Agent, before training
-mean_reward_before_train = evaluate(model, num_episodes=100, deterministic=True)
 
-mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100, warn=False)
+env = gym.make("CartPole-v1")
+
+model = PPO(MlpPolicy, env, verbose=0)
+
+# Random Agent, before training
+mean_reward_before_train = evaluate(
+    model, num_episodes=100, deterministic=True)
+
+mean_reward, std_reward = evaluate_policy(
+    model, env, n_eval_episodes=100, warn=False)
 
 print(f"mean_reward: {mean_reward:.2f} +/- {std_reward:.2f}")
 
@@ -72,56 +73,24 @@ mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
 
 print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
 
-def show_videos(video_path="", prefix=""):
-    """
-    Taken from https://github.com/eleurent/highway-env
+save_dir = "/tmp/gym/"
+os.makedirs(save_dir, exist_ok=True)
 
-    :param video_path: (str) Path to the folder containing videos
-    :param prefix: (str) Filter the video, showing only the only starting with this prefix
-    """
-    html = []
-    for mp4 in Path(video_path).glob("{}*.mp4".format(prefix)):
-        video_b64 = base64.b64encode(mp4.read_bytes())
-        html.append(
-            """<video alt="{}" autoplay 
-                    loop controls style="height: 400px;">
-                    <source src="data:video/mp4;base64,{}" type="video/mp4" />
-                </video>""".format(
-                mp4, video_b64.decode("ascii")
-            )
-        )
-    # ipythondisplay.display(ipythondisplay.HTML(data="<br>".join(html)))
+# The model will be saved under PPO_tutorial.zip
+model.save(f"{save_dir}/PPO_tutorial")
 
+# sample an observation from the environment
+obs = model.env.observation_space.sample()
 
+# Check prediction before saving
+print("pre saved", model.predict(obs, deterministic=True))
 
+del model  # delete trained model to demonstrate loading
 
-def record_video(env_id, model, video_length=500, prefix="", video_folder="videos/"):
-    """
-    :param env_id: (str)
-    :param model: (RL model)
-    :param video_length: (int)
-    :param prefix: (str)
-    :param video_folder: (str)
-    """
-    eval_env = DummyVecEnv([lambda: gym.make(env_id, render_mode="rgb_array")])
-    # Start the video at step=0 and record 500 steps
-    eval_env = VecVideoRecorder(
-        eval_env,
-        video_folder=video_folder,
-        record_video_trigger=lambda step: step == 0,
-        video_length=video_length,
-        name_prefix=prefix,
-    )
+loaded_model = PPO.load(f"{save_dir}/PPO_tutorial")
+# Check that the prediction is the same after loading (for the same observation)
+print("loaded", loaded_model.predict(obs, deterministic=True))
 
-    obs = eval_env.reset()
-    for _ in range(video_length):
-        action, _ = model.predict(obs)
-        obs, _, _, _ = eval_env.step(action)
-
-    # Close the video recorder
-    eval_env.close()
-
-
-record_video("CartPole-v1", model, video_length=500, prefix="ppo-cartpole")
+# record_video("CartPole-v1", model, video_length=500, prefix="ppo-cartpole")
 
 # show_videos("videos", prefix="ppo")
