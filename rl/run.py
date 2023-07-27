2cbdc8688c507929e895f1d2539a7155bad6b820
hansen1416 <badapplesweetie@gmail.com>
2023-07-27 11:38:54 +0800

    1

--- rl/run.py
+++ rl/run.py
@@ -4,6 +4,14 @@ from stable_baselines3 import PPO
 from stable_baselines3.ppo.policies import MlpPolicy
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
+# Set up fake display; otherwise rendering will fail
+import os
+import base64
+from pathlib import Path
+
+os.system("Xvfb :1 -screen 0 1024x768x24 &")
+os.environ['DISPLAY'] = ':1'
 
 env = gym.make("CartPole-v1")
 
@@ -63,3 +71,57 @@ model.learn(total_timesteps=10_000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
 
 print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
+
+def show_videos(video_path="", prefix=""):
+    """
+    Taken from https://github.com/eleurent/highway-env
+
+    :param video_path: (str) Path to the folder containing videos
+    :param prefix: (str) Filter the video, showing only the only starting with this prefix
+    """
+    html = []
+    for mp4 in Path(video_path).glob("{}*.mp4".format(prefix)):
+        video_b64 = base64.b64encode(mp4.read_bytes())
+        html.append(
+            """<video alt="{}" autoplay 
+                    loop controls style="height: 400px;">
+                    <source src="data:video/mp4;base64,{}" type="video/mp4" />
+                </video>""".format(
+                mp4, video_b64.decode("ascii")
+            )
+        )
+    # ipythondisplay.display(ipythondisplay.HTML(data="<br>".join(html)))
+
+
+
+
+def record_video(env_id, model, video_length=500, prefix="", video_folder="videos/"):
+    """
+    :param env_id: (str)
+    :param model: (RL model)
+    :param video_length: (int)
+    :param prefix: (str)
+    :param video_folder: (str)
+    """
+    eval_env = DummyVecEnv([lambda: gym.make(env_id, render_mode="rgb_array")])
+    # Start the video at step=0 and record 500 steps
+    eval_env = VecVideoRecorder(
+        eval_env,
+        video_folder=video_folder,
+        record_video_trigger=lambda step: step == 0,
+        video_length=video_length,
+        name_prefix=prefix,
+    )
+
+    obs = eval_env.reset()
+    for _ in range(video_length):
+        action, _ = model.predict(obs)
+        obs, _, _, _ = eval_env.step(action)
+
+    # Close the video recorder
+    eval_env.close()
+
+
+record_video("CartPole-v1", model, video_length=500, prefix="ppo-cartpole")
+
+# show_videos("videos", prefix="ppo")
