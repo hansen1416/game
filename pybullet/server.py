80c6e9038d2c018e382e2f3033e1c7e032d2a427
hansen1416 <badapplesweetie@gmail.com>
2023-08-06 19:20:58 +0800

    1

---
+++ pybullet/server.py
@@ -0,0 +1,10 @@
+import os
+import pybullet as p
+import pybullet_data
+
+p.connect(p.GUI)
+pandaUid = p.loadURDF(os.path.join(
+    pybullet_data.getDataPath(), "franka_panda/panda.urdf"), useFixedBase=True)
+
+while True:
+    p.stepSimulation()
