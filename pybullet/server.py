5ef0283239ca49e2eb6975cfb8e6130916ace177
hansen1416 <badapplesweetie@gmail.com>
2023-08-06 19:22:57 +0800

    1

--- pybullet/server.py
+++ pybullet/server.py
@@ -2,7 +2,7 @@ import os
 import pybullet as p
 import pybullet_data
 
-p.connect(p.GRAPHICS_SERVER)
+p.connect(p.DIRECT)
 pandaUid = p.loadURDF(os.path.join(
     pybullet_data.getDataPath(), "franka_panda/panda.urdf"), useFixedBase=True)
 
