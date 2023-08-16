af5a1d4623c5598cc4efc5a85b6ccc983465a9e2
hansen1416 <badapplesweetie@gmail.com>
2023-08-16 22:15:23 +0800

    1

--- pybullet/server.py
+++ pybullet/server.py
@@ -12,30 +12,30 @@ from dataclasses import dataclass
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-
 @dataclass
 class Joint:
-  index: int
-  name: str
-  type: int
-  gIndex: int
-  uIndex: int
-  flags: int
-  damping: float
-  friction: float
-  lowerLimit: float
-  upperLimit: float
-  maxForce: float
-  maxVelocity: float
-  linkName: str
-  axis: tuple
-  parentFramePosition: tuple
-  parentFrameOrientation: tuple
-  parentIndex: int
+    index: int
+    name: str
+    type: int
+    gIndex: int
+    uIndex: int
+    flags: int
+    damping: float
+    friction: float
+    lowerLimit: float
+    upperLimit: float
+    maxForce: float
+    maxVelocity: float
+    linkName: str
+    axis: tuple
+    parentFramePosition: tuple
+    parentFrameOrientation: tuple
+    parentIndex: int
 
-  def __post_init__(self):
-    self.name = str(self.name, 'utf-8')
-    self.linkName = str(self.linkName, 'utf-8')
+    def __post_init__(self):
+        self.name = str(self.name, 'utf-8')
+        self.linkName = str(self.linkName, 'utf-8')
+
 
 def render(pybullet_scene):
     width = 320
@@ -79,13 +79,14 @@ def _train():
 
     env.reset()
 
-    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=os.path.join(PROJECT_DIR, 'logs'))
+    model = PPO('MlpPolicy', env, verbose=1,
+                tensorboard_log=os.path.join(PROJECT_DIR, 'logs'))
 
-    TIMESTEPS=10000
+    TIMESTEPS = 10000
 
     model.learn(total_timesteps=TIMESTEPS,
                 reset_num_timesteps=False, tb_log_name=f"{TIMESTEPS}")
-    
+
 
 def demo():
 
@@ -93,7 +94,6 @@ def demo():
 
     # The module pybullet_data provides many example Universal Robotic Description Format (URDF) files.
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
 
     # car_urdf = os.path.join(PROJECT_DIR, "urdf", "simplecar.urdf")
     # car_id = p.loadURDF(fileName=car_urdf,
@@ -107,24 +107,21 @@ def demo():
 
     plane_urdf = os.path.join(PROJECT_DIR, "urdf", "simpleplane.urdf")
     planeId = p.loadURDF(fileName=plane_urdf,
-                        basePosition=[0, 0, 0.1],
-                        physicsClientId=client_id)
+                         basePosition=[0, 0, 0.1],
+                         physicsClientId=client_id)
 
-    n_joints = p.getNumJoints(arm_id, physicsClientId=client_id)
+    # n_joints = p.getNumJoints(arm_id, physicsClientId=client_id)
 
-    print("==========joints info=========")
+    # print("==========joints info=========")
 
-    for i in range(n_joints):
-        joint = Joint(*p.getJointInfo(arm_id, i, physicsClientId=client_id))
-        print(joint)
+    # for i in range(n_joints):
+    #     joint = Joint(*p.getJointInfo(arm_id, i, physicsClientId=client_id))
+    #     print(joint)
 
-    # p.setJointMotorControlArray(arm_id, [0,1],
-    #                             controlMode=p.POSITION_CONTROL,
-    #                             targetPositions=[1, 5],
-    #                             physicsClientId=client_id)
-
-    
-
+    p.setJointMotorControlArray(arm_id, [0],
+                                controlMode=p.POSITION_CONTROL,
+                                targetPositions=[2],
+                                physicsClientId=client_id)
 
     # action_space = gym.spaces.box.Box(low=np.array([0, -.6], dtype=np.float32),high=np.array([1, .6], dtype=np.float32))
 
